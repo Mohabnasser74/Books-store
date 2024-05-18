430c46f216ce2e2aec64bd588b4a535ed0c3737a
Mohab <114109820+Mohabnasser74@users.noreply.github.com>
2024-05-18 05:38:12 +0300

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = (_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://books-store-api-8uzz.onrender.com");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization "
