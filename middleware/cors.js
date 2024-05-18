0ca1059c6d249403eb5219cc510c8ef5354e1c44
Mohab <114109820+Mohabnasser74@users.noreply.github.com>
2024-05-18 05:44:51 +0300

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = (_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://books-store1st.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://books-store1st.netlify.app/bb");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization "
