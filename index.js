af3a79d363c22ac05c154ad31d5082d4caac6b4d
Mohab <114109820+Mohabnasser74@users.noreply.github.com>
2024-05-19 23:35:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,13 +43,16 @@ app.all("*", (_, res) => {
   });
 });
 
-// DB
-async function main() {
-  await mongoose.connect(process.env.URI_CONNECTION);
-  console.log("mongoose was started");
-  app.listen(process.env.PORT, () => {
-    console.log(`App is listening to port: ${process.env.PORT}`);
-  });
-}
-
-main().catch((err) => console.log(err));
+(async () => {
+  try {
+    await mongoose.connect(process.env.URI_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+  }
+})();
